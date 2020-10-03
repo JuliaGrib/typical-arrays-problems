611a0143369c15b3584f9fc9b5d15b256bbbbf10
Julia Grib <yulgrib@gmail.com>
2020-10-03 17:45:48 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array) {
+    return 0;
+  } 
+  else if (array.lenght === 0) {
+    return 0;
+  }
+  else if (array == 0) {
+    return 0;
+  }    
+  else {
+    let minInt = Math.min.apply(null, array);
+    return minInt;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array) {
+    return 0;
+  } 
+  else if (array.lenght === 0) {
+    return 0;
+  }
+  else if (array == 0) {
+    return 0;
+  }    
+  else {
+    let maxInt = Math.max.apply(null, array);
+    return maxInt;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (!array) {
+    return 0;
+  } 
+  else if (array.lenght === 0) {
+    return 0;
+  }
+  else if (array == 0) {
+    return 0;
+  }    
+  else {
+    let total = 0;
+    for(var i = 0; i < array.length; i++) {
+      total += array[i];
+  }
+  let avgInt = total / array.length;
+  return avgInt;
+  }
+    
+ }
